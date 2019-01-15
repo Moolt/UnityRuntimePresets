b7734e0f7094ca81179ba6d7e173a1de9e0ebed2
Malte Weiß <malteweiss@yahoo.de>
2019-01-15 22:34:52 +0100

    adds basic implementation

---
+++ Assets/Scripts/RuntimePresets/Editor/CreateRuntimePresetEditor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+
+namespace RuntimePresets
+{
+    public class CreateRuntimePresetEditor
+    {
+        [MenuItem("CONTEXT/Component/Create Runtime Preset")]
+        private static void CreateRuntimePreset(MenuCommand command)
+        {
+            var sourceComponent = (Component)command.context;
+            var dummyObject = new GameObject();
+
+            var targetComponent = dummyObject.GetOrCreateComponent(sourceComponent.GetType());
+            var runtimePreset = dummyObject.GetOrCreateComponent<RuntimePreset>();
+            runtimePreset.TargetComponent = targetComponent;
+
+            UnityEditorInternal.ComponentUtility.CopyComponent(sourceComponent);
+            UnityEditorInternal.ComponentUtility.PasteComponentValues(targetComponent);
+
+            var prefab = PrefabUtility.CreatePrefab("Assets/New Preset.prefab", dummyObject);
+            Object.DestroyImmediate(dummyObject);
+            //TODO: Implement object picker
+            //EditorGUIUtility.ShowObjectPicker<GameObject>(null, false, string.Empty, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/RuntimePresets/GameObjectExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RuntimePresets
+{
+    public static class GameObjectExtensions
+    {
+        public static Component GetOrCreateComponent(this GameObject target, Type type)
+        {
+            var component = target.GetComponent(type);
+
+            if (component == null)
+            {
+                component = target.AddComponent(type);
+            }
+
+            return component;
+        }
+
+        public static T GetOrCreateComponent<T>(this GameObject target) where T : Component
+        {
+            return (T)target.GetOrCreateComponent(typeof(T));
+        }
+    }
+}
