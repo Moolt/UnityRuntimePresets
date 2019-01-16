102572bf022ead6ad6a34a064cb8710dbf7c3b01
Malte Weiß <malteweiss@yahoo.de>
2019-01-16 19:59:21 +0100

    adds examples

---
+++ Assets/Examples/Scripts/ApplyPreset.cs
@@ -0,0 +1,13 @@
+﻿using RuntimePresets;
+using UnityEngine;
+
+public class ApplyPreset : MonoBehaviour
+{
+    public Preset _preset;
+    public Light _light;
+
+    private void Awake()
+    {
+        _preset.ApplyTo(_light);
+    }
+}
---
+++ Assets/Examples/Scripts/CreatePreset.cs
@@ -0,0 +1,17 @@
+﻿using RuntimePresets;
+using UnityEngine;
+
+public class CreatePreset : MonoBehaviour
+{
+    public Light otherLight;
+    private IPreset _preset;
+
+    void Start()
+    {
+        var light = GetComponent<Light>();
+        _preset = Preset.From(otherLight);
+        _preset.ApplyTo(light);
+        //Removes the temporary object created to store the runtime preset
+        _preset.Free();        
+    }
+}
